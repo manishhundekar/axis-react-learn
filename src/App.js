7100415862c2176602e60d5323c2406c20e24dc6
Manish <manishhundekar@gmail.com>
2022-01-31 12:36:36 +0530

    Day 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import Form from "./pages/Form";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/form" element={<Form />} />
       </Routes>
     </BrowserRouter>
   );
